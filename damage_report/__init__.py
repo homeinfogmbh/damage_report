4a51d38b9ed9645ed212f28da859e0f12a6c65c5
Richard Neumann <r.neumann@homeinfo.de>
2018-10-02 13:20:33 +0200

    Added missing exports.

--- damage_report/__init__.py
+++ damage_report/__init__.py
@@ -1,5 +1,8 @@
 """Microservice for damage reports."""
 
+from damage_report.email import email
+from damage_report.orm import DamageReport
 from damage_report.wsgi import APPLICATION
 
-__all__ = ['APPLICATION']
+
+__all__ = ['APPLICATION', 'email', 'DamageReport']
