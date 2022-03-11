f16708b162058e30cbadba6ad902a29e43466d9b
Richard Neumann <r.neumann@homeinfo.de>
2022-03-11 10:24:31 +0100

    Outsource email creation into own function

--- damage_report/email.py
+++ damage_report/email.py
@@ -22,26 +22,35 @@ def get_emails(damage_report: Union[DamageReport, int]) -> Iterator[EMail]:
     for notification_email in NotificationEmail.select().where(
             NotificationEmail.customer == damage_report.customer
     ):
-        recipient = notification_email.email
-        sender = (config := get_config()).get('email', 'from')
-        subject = notification_email.subject or config.get('email', 'subject')
-        subject = subject.format(
-            damage_type=damage_report.damage_type,
-            address=damage_report.address
-        )
-        message = (
-            f'Schadensmeldung "{damage_report.damage_type}" von '
-            f'{damage_report.name} ({damage_report.contact}):'
-        )
+        yield create_email(damage_report, notification_email)
 
-        if notification_email.html:
-            html = message + '<br/><br/>' + damage_report.message
-            plain = None
-        else:
-            plain = message + '\n\n' + damage_report.message
-            html = None
 
-        yield EMail(subject, sender, recipient, plain=plain, html=html)
+def create_email(
+        damage_report: DamageReport,
+        notification_email: NotificationEmail
+) -> EMail:
+    """Creates an email for a damage report."""
+
+    recipient = notification_email.email
+    sender = (config := get_config()).get('email', 'from')
+    subject = notification_email.subject or config.get('email', 'subject')
+    subject = subject.format(
+        damage_type=damage_report.damage_type,
+        address=damage_report.address
+    )
+    message = (
+        f'Schadensmeldung "{damage_report.damage_type}" von '
+        f'{damage_report.name} ({damage_report.contact}):'
+    )
+
+    if notification_email.html:
+        html = message + '<br/><br/>' + damage_report.message
+        plain = None
+    else:
+        plain = message + '\n\n' + damage_report.message
+        html = None
+
+    yield EMail(subject, sender, recipient, plain=plain, html=html)
 
 
 email = get_email_func(get_emails)
