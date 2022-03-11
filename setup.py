31eb28db6ea5150e9fe71d64d7eee5ed72375dc7
Richard Neumann <r.neumann@homeinfo.de>
2022-03-11 10:13:37 +0100

    Migrate to setuptools

--- setup.py
+++ setup.py
@@ -1,15 +1,29 @@
 #! /usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
     name='damage_report',
-    version='latest',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
     author_email='<info at homeinfo dot de>',
     maintainer='Richard Neumann',
-    maintainer_email='<r dot neumann at homeinfo priod de>',
+    maintainer_email='<r dot neumann at homeinfo period de>',
+    install_requires=[
+        'configlib',
+        'emaillib',
+        'filedb',
+        'flask',
+        'his',
+        'mdb',
+        'notificationlib',
+        'peewee',
+        'peeweeplus'
+    ],
     packages=['damage_report'],
-    license='GPLv3',
-    description='HIS microservice to handle damage reports.')
+    description='HIS microservice to handle damage reports.'
+)
